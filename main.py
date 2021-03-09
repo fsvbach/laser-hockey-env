e2aaec7fc2d02f38fe6431548d5a04cb9fb2e576
JohannesSchulz97 <schulz.johannes97@gmail.com>
2021-03-09 11:14:16 +0100

    new weights

--- main.py
+++ main.py
@@ -18,31 +18,35 @@ env = h_env.HockeyEnv()
 attack = h_env.HockeyEnv(mode=h_env.HockeyEnv.TRAIN_SHOOTING)
 defense = h_env.HockeyEnv(mode=h_env.HockeyEnv.TRAIN_DEFENSE)
 
-name='attack'
+load_weights='attack'
+store_weights='basic_opponent'
 
 q_agent = agent.DQNAgent(env.observation_space, 
                          Discrete(8),
                         convert_func =  env.discrete_to_continous_action,
-                        pretrained   = f'DQN/weights/{name}', train=True)
+                        pretrained   = f'DQN/weights/{load_weights}', train=True)
 
 
 ddpg_player = DDPGAgent(env.observation_space, 
                          env.action_space)                
 
-losses, rewards = training.train(attack, q_agent, player2=False, name=name, max_episodes=100000)
+player2 = h_env.BasicOpponent()
 
-# plt.plot(training.running_mean(losses,64))
-# plt.savefig(f'Plots/{name}_losses')
-# plt.show()
-# plt.close()
 
-# plt.plot(training.running_mean(rewards,64))
-# plt.savefig(f'Plots/{name}_rewards')
-# plt.show()
-# plt.close()
+losses, rewards = training.train(env, q_agent, player2=player2, name=store_weights, max_episodes=10000)
 
-# player2 = h_env.BasicOpponent()
-stats = gameplay(attack, q_agent, player2=False, N=10, show=True, analyze=False)
+plt.plot(training.running_mean(losses,64))
+plt.savefig(f'Plots/{store_weights}_losses')
+plt.show()
+plt.close()
+
+plt.plot(training.running_mean(rewards,64))
+plt.savefig(f'Plots/{store_weights}_rewards')
+plt.show()
+plt.close()
+
+
+stats = gameplay(attack, q_agent, player2=False, N=30, show=True, analyze=False)
 print(stats)
 
 defense.close()
