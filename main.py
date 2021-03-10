5e6c9f958e8a559e4553e59883dccfba3900378c
fsvbach <simmiasmobil@gmail.com>
2021-03-10 11:35:26 +0100

    game

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ q_agent = agent.DQNAgent(env.observation_space,
                         convert_func =  env.discrete_to_continous_action,
                         pretrained   = f'DQN/weights/{name}')
 
+ddpg = DDPGAgent(env,
+                         actor_lr=1e-4,
+                         critic_lr=1e-3,
+                         update_rate=0.05,
+                         discount=0.9, update_target_every=20,
+                         pretrained='DDPG/weights/ddpg-defense-eps-noise-trained-10000')
+
 td3 = TD3(18, 4, 1.0, env)
 td3.load(filename='next')
 
@@ -37,7 +44,7 @@ td3.load(filename='next')
 #                                  max_episodes=50000)
 
 
-stats = gameplay(env, q_agent, player2=td3, N=5, show=True, analyze=False)
+stats = gameplay(env, ddpg, player2=td3, N=5, show=True, analyze=False)
 print(stats)
 
 env.close()
