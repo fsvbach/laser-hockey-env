89f053a0a8843e87d0ac3d4278a20616716bd1ee
JohannesSchulz97 <schulz.johannes97@gmail.com>
2021-03-15 09:41:51 +0100

    deleting stupid weights

--- main_dqn.py
+++ main_dqn.py
@@ -24,7 +24,7 @@ defense = h_env.HockeyEnv(mode=h_env.HockeyEnv.TRAIN_DEFENSE)
 
 
 env = h_env.HockeyEnv()
-load_weights = 'training_hall:50000_omega=110_1_150_75_10_40000'
+load_weights = 'alg3'
 #load_weights = 'training_hall_1'
 
 td3 = TD3(pretrained='stronger')
@@ -39,7 +39,7 @@ q_agent = agent.DQNAgent(env.observation_space, env.discrete_action_space,
                         pretrained   = f'DQN/weights/{load_weights}')
 
 
-stats = gameplay(env, q_agent, player2=strong_basic_opponent, N=10, show=True, analyze=False)
+stats = gameplay(env, q_agent, player2=weak_basic_opponent, N=100, show=False, analyze=False)
 print("ties-wins-losses: ", stats)
 env.close()
 
@@ -129,8 +129,9 @@ env.close()
 # TRAINING HALL
 
 
-#load_weights = 'training_hall:50000_omega=110_1_150_75_10'
-# load_weights = 'exp2_15000'
+# load_weights = 'training_hall:50000_omega=110_1_150_75_10'
+# load_weights = ''
+
 # training_hall = TrainingHall2()
 
 # td3 = TD3(pretrained='stronger')
@@ -163,10 +164,9 @@ env.close()
 
 
 # # weak basic opponent is in training per default
-# training_hall.register_opponents([strong_basic_opponent])
 
 # #store_weights = f'training_hall:50000_omega=1{q_agent._config["winner"]}_{q_agent._config["positioning"]}_{q_agent._config["distance_puck"]}_{q_agent._config["puck_direction"]}_{q_agent._config["touch_puck"]}'
-# store_weights = "exp3"
+# store_weights = "exp4"
 
 # #losses, rewards = training.train(normal, q_agent, player2=basic_opponent, name=store_weights, show=False, max_episodes=10000)
 # losses, rewards = training.train(training_hall, q_agent, name=store_weights, show=False, max_episodes=10000)
